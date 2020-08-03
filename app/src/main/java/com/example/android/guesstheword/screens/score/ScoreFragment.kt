06dc86ac699b12d410edefd119258d56f8598323
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-04 00:26:03 +0700

    Add LiveData to the ScoreViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -53,6 +53,12 @@ class ScoreFragment : Fragment() {
         viewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(ScoreViewModel::class.java)
 
+        // Add observer for score
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+
+        binding.scoreText.text = viewModel.score.toString()
         binding.scoreText.text = viewModel.score.toString()
 
         return binding.root
