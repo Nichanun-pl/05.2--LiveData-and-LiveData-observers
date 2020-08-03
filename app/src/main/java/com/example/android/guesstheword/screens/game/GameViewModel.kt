e0049e3a44ba6faa7e9d9f7476f4561da8956ffe
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-04 00:15:38 +0700

    Add a backing property to score and word

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,6 +17,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -29,7 +30,9 @@ class GameViewModel : ViewModel() {
     var word = MutableLiveData<String>()
 
     // The current score
-    var score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
